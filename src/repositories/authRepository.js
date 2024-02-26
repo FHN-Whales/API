66a8d3e4275b9e5ab08f01eb1f3dd9cc07f422ed
Trung Dang <trung.dang24@studen.passerellesnumeriques.org>
2024-02-27 00:22:57 +0700

    fix error at line 65

--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -32,8 +32,7 @@ exports.signUp = async (userData) => {
       completed: true,
       message: 'We have successfully sent the new verification code to your email',
       userId: existingEmail.id,
-      abc:"12321"
-
+      abc:"123"
     };
 
   }
@@ -62,8 +61,7 @@ exports.signUp = async (userData) => {
     return {
       completed: true,
       message: 'We have successfully sent the verification code to your email',
-      userId: existingEmail.id,
-      abc:"12321"
+      abc:"123111111"
     };
   }
 };
